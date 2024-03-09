5c09bd957d6c955a37810ca2d601e639e7cb0b1a
Stephen Boettcher <sboettcher@pch.com>
2024-03-09 16:36:56 -0500

    addsetup form and other updates

---
+++ src/formValid.js
@@ -0,0 +1,19 @@
+// Check for valid format
+function validateWords(value) {
+    const words = value.replace(/\s/g, "").split(',');
+    
+    console.log(words.length);
+    
+    // Check each word
+    const isValid = words.every(word => {
+      console.log(word);
+      return /^[a-zA-Z]+$/.test(word); 
+    });
+    
+    return isValid ? words : [];
+  }
+
+const validateForm = ({name, words}) => {    
+    return { name, words: validateWords(words) };    
+};
+export default validateForm;
