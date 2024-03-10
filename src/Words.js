18bbfa4bdf09aa6cd5049013f9e68418299a1052
Stephen Boettcher <sboettcher@pch.com>
2024-03-10 12:04:55 -0400

    updates

--- src/Words.js
+++ src/Words.js
@@ -2,9 +2,15 @@ class Words {
     constructor(words) {
         this.words = words;        
     }
+    
+    update(words){
+        console.log("Update: ", words);
+        this.words = words;
+        return this;  
+    }
             
     output() {
-        return `${this.words.map(word => `<div>${word.clean}</div>`).join('')}`;
+        return this.words.length ? `${this.words.map(word => `<div>${word.clean}</div>`).join('')}` : '';
     }
 }
 
